daf7751c17ac9463594c72eb00863090797bde0b
Vinay Mandal <i.m.vinaym@gmail.com>
2012-05-30 15:23:25 -0400

    Project Euler 5

---
+++ src/Euler5.scala
@@ -0,0 +1,13 @@
+object Euler5 extends App {    
+    def divideByPrime(prime:Int, lst:List[Int], primes:List[Int]):(Int, List[Int], List[Int]) = {      
+      (lst.find(_ % prime == 0)) match {
+        case Some(list) => divideByPrime(prime, lst.map(x => if(x%prime ==0) x/prime else x).filter(_ != 1), prime::primes)
+        case _ => (prime, lst, primes)
+      }
+    }
+    val numbers = 2 to 20 toList
+    val primes = List(2, 3, 5, 7, 11, 13, 17, 19)    
+    val result = primes.foldLeft((1, numbers, List(1)))((lst, i) => divideByPrime(i, lst._2, lst._3) )
+    val lcd = (result._3).foldLeft(1)(_*_)    
+    println("result " + lcd)  
+}
